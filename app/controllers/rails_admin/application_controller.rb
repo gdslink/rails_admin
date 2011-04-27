2d93d04b3d5523c3edc9a78ba9ae940f6aeab703
Jean-Dominique Morani <jdmorani@gmail.com>
2011-04-27 09:17:39 -0500

    fix get_scope_models method name

--- app/controllers/rails_admin/application_controller.rb
+++ app/controllers/rails_admin/application_controller.rb
@@ -6,7 +6,7 @@ module RailsAdmin
     before_filter :_authorize!
     before_filter :_scope!    
     before_filter :set_plugin_name
-    before_filter :get_scope_models
+    before_filter :_get_scope_models!
 
     helper_method :_current_user
 
@@ -35,7 +35,7 @@ module RailsAdmin
     end
 
     def _get_scope_models!
-      @scope_adapter.get_scope_models if @scope_adapter
+      get_scope_models if @scope_adapter
     end
 
     def _authenticate!
