da5b615482a112a79caf99504d01229effcfc614
Jean-Dominique Morani <jdmorani@gmail.com>
2011-04-27 23:43:02 -0500

    Implemented more robust scope builder

--- lib/rails_admin/extensions/scope/scope_adapter.rb
+++ lib/rails_admin/extensions/scope/scope_adapter.rb
@@ -1,6 +1,9 @@
 module RailsAdmin
   module Extensions
     module Scope
+      
+
+      
       # This adapter enables a scope selector that limits the records fetched from the DB
       # to the specified scope.
       class ScopeAdapter
@@ -10,7 +13,7 @@ module RailsAdmin
           @controller = controller
           @controller.extend ControllerExtension
         end
-         
+        
         # Array of the models defined as part of the scope configuration
         # Ex:
         # RailsAdmin.scope_with :scope, [Company, Application]
@@ -18,8 +21,8 @@ module RailsAdmin
         #
         def models
           @models
-        end       
-         
+        end        
+        
         # Apply the scope to an ActiveRelation object
         # First argument is the query the scope has to be applied to
         # Second argument is the base_model that is used in the select statement.
@@ -30,28 +33,24 @@ module RailsAdmin
             next if base_abstract_model.model.name == key
             tree = retrieve_associations_tree(base_abstract_model, key)
             next if not tree
-            tree.each do |model|
-              next if model == base_abstract_model.model
-              #we check if the tree has only one association.
-              #We check the length against 2 because the first element is always
-              #the main model, the next object in the array is the first association etc...
-              if tree.length == 2 then
-                query = query.where("#{model.table_name.singularize}_id = #{value}")
-              else
-                case model.name
-                when key
-                  query = query.where("#{model.table_name.singularize}_id = #{value}")
-                else
-                  query = query.joins(model.table_name.singularize.to_sym)
-                end
-              end       
-            end
+            #remove the first element since it's always going to be the abstract base model
+            tree.shift
+            
+            #build the joins query            
+            query = query.joins([tree.collect{|model| model.table_name.singularize.to_sym}.reverse.inject { |a, b| {b => a}}])
+
+            #build the conditions based on the selected scope
+            @controller.current_scope.each do |key, value|
+              if(tree.collect{|model| model.name}.include?(key))
+                query = query.where(key.constantize.table_name.to_sym => {:id => value})
+              end
+            end    
           end
           query
         end
-         
+        
         private
-         
+        
         # Retrieve the association hierarchy for a model.
         # Ex: let's assume a model is defined as follow :
         # Field belongs_to Table belongs_to Company
@@ -62,34 +61,41 @@ module RailsAdmin
           abstract_model.associations.each do |assoc|
             abstract_model = RailsAdmin::AbstractModel.new(assoc[:parent_model].name)
             retrieve_associations_tree(abstract_model, association_name, tree) if not tree.include?(abstract_model.model)        
-          end
+          end          
           tree = nil if not tree.include?(association_name.constantize) rescue nil
           tree
         end
-                         
+                        
         module ControllerExtension
-           
+          
           def current_scope
             # use _current_user instead of default current_user so it works with
             # whatever current user method is defined with RailsAdmin
             @current_scope = session[:scope]
           end
-           
+          
           def update_scope
             model = @scope_adapter.models[@scope_adapter.models.index { |model| params[:model] == model.name }]
-            session[:scope][model.name] = params[:selected]
+            first_id = model.first.id
+            record_ids  = @scope[model.name][:entries].collect{ |e| e.id}
+            p record_ids
+            if not record_ids.include?(params[:selected].to_i) then
+              session[:scope][model.name] = first_id
+            else
+              session[:scope][model.name] = params[:selected]
+            end
             get_scope_models
             respond_to do |format|
               format.js {render :partial => 'rails_admin/extensions/scope/scope_selector', :locals => {:models => @scope_adapter.models}}
             end
           end
-           
+          
           def list_entries_for(model_name, association = {})
             abstract_model = RailsAdmin::AbstractModel.new(model_name)
             scope = @authorization_adapter && @authorization_adapter.query(:list, abstract_model)
             abstract_model.where(association, scope)
           end
-           
+                    
           def get_scope_models
             @scope = {}
             session[:scope] ||= {}
@@ -103,7 +109,7 @@ module RailsAdmin
               parent_model = model        
               parent_selection = session[:scope][model_name]
             end
-          end         
+          end          
         end
       end
     end
