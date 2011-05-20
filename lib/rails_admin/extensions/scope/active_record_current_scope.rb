cbd9e73dc35c11efae4f51ef907a3954f79b45d2
Jean-Dominique Morani <jdmorani@gmail.com>
2011-05-20 11:34:48 -0500

    fixed comment in active_record_current_scope

--- lib/rails_admin/extensions/scope/active_record_current_scope.rb
+++ lib/rails_admin/extensions/scope/active_record_current_scope.rb
@@ -4,21 +4,8 @@ module RailsAdmin
   module DefaultScope
     module ClassMethods
       # Returns a scope which fetches only the records that the passed ability
-      # can perform a given action on. The action defaults to :index. This
-      # is usually called from a controller and passed the +current_ability+.
-      #
-      #   @articles = Article.accessible_by(current_ability)
-      #
-      # Here only the articles which the user is able to read will be returned.
-      # If the user does not have permission to read any articles then an empty
-      # result is returned. Since this is a scope it can be combined with any
-      # other scopes or pagination.
-      #
-      # An alternative action can optionally be passed as a second argument.
-      #
-      #   @articles = Article.accessible_by(current_ability, :update)
-      #
-      # Here only the articles which the user can update are returned.
+      # can perform a given action on. 
+      # authorization_adapter and scope_adapter are instance variables set at the controller level.
       def current_scope(authorization_adapter, scope_adapter)
         abstract_model = RailsAdmin::AbstractModel.new(self.name)
         scope = authorization_adapter && authorization_adapter.query(:read, abstract_model)
