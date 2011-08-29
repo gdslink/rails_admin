b5c146a946d7b2c960a4211c610fd98b759a6ef0
Jean-Dominique Morani <jdmorani@gmail.com>
2011-08-29 16:29:10 -0500

    add application permission to admin role

--- app/models/admin_role.rb
+++ app/models/admin_role.rb
@@ -1,14 +1,15 @@
 class AdminRole < ActiveRecord::Base
   has_and_belongs_to_many :admin_permissions, :join_table => :admin_roles_admin_permissions
-  has_and_belongs_to_many :users, :join_table => :users_roles
   has_and_belongs_to_many :applications, :join_table => :applications_admin_roles
+  has_and_belongs_to_many :users, :join_table => :users_roles  
 
   rails_admin do
     edit do
       field :name
       field :description
-      group :permissions do
+      group :permissions do        
         label "Permissions"
+        field :applications
         field :admin_permissions do
           label "Permissions"
         end
