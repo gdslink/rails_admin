bc3c5ed3d14f7d91243c47bb74407d1dc5570477
Jean-Dominique Morani <jdmorani@gmail.com>
2011-09-13 10:56:35 -0500

    Finally fixed the long time bug of the top submit bar not working :-)

--- app/assets/javascripts/rails_admin/application.js
+++ app/assets/javascripts/rails_admin/application.js
@@ -4,4 +4,15 @@ if (typeof($j) === "undefined" && typeof(jQuery) !== "undefined") {
 
 $j(document).ready(function($){
   $(".ra-button").not(".ui-button").button({});
+
+
+  if ($("#submit-buttons").length > 0 && $("#submit-buttons").children().length == 0){
+    var form = $(".remove-for-form.ra-block-toolbar.ui-state-highlight.clearfix input[type='submit']").parents('form:first');
+    $("ul.submit").clone(true).appendTo("#submit-buttons");    
+    $("#submit-buttons input[type='submit']").click(function(){
+      form.append('<input type="hidden" name="' + $(this).attr('name') + '"/>');
+      form.submit();
+    })
+  }
+
 });
