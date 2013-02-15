6d434b00750c90c60a1986fb7ceeb82e2a07c684
jdmorani <jdmorani@gmail.com>
2013-02-15 20:05:37 +0100

    make sure when the user login if he is associated to a specific company we select that company by default instead of the first available company in the list of companies he has access too

--- lib/rails_admin/extensions/scope/scope_adapter.rb
+++ lib/rails_admin/extensions/scope/scope_adapter.rb
@@ -1,7 +1,7 @@
 module RailsAdmin
   module Extensions
     module Scope
-      
+
       # This adapter enables a scope selector that limits the records fetched from the DB
       # to the specified scope.
       class ScopeAdapter
@@ -11,7 +11,7 @@ module RailsAdmin
           @controller = controller
           @controller.extend ControllerExtension
         end
-        
+
         # Array of the models defined as part of the scope configuration
         # Ex:
         # RailsAdmin.scope_with :scope, [Company, Application]
@@ -20,14 +20,14 @@ module RailsAdmin
         def models
           @models
         end
-        
+
         # Apply the scope to an ActiveRelation object
         # First argument is the query the scope has to be applied to
         # Second argument is the base_model that is used in the select statement.
         # Example: select field from table (table is the base_model).
         def apply_scope(query, base_abstract_model)
           join, tree = nil
-          
+
           #Treat the User model as a special case since we want to see all the users if they are not hierarchically
           #assigned (ex: User not being associated to a company)
           # if @controller.current_user.class.name == base_abstract_model.model.name then
@@ -36,11 +36,11 @@ module RailsAdmin
 
           @controller.current_scope.each do |key, value|
             next if base_abstract_model.model.name == key
-            
+
             tree = retrieve_associations_tree(base_abstract_model, key)
-            
+
             next if not tree
-            
+
             join = generate_join_query(key, tree[base_abstract_model.model.name])
 
             query = query.includes(join)
@@ -62,9 +62,9 @@ module RailsAdmin
               else
                 predicate_or << ({assoc[:child_key].to_s => nil})
               end
-            end            
-            
-            predicate = predicate.inject(:&)                         
+            end
+
+            predicate = predicate.inject(:&)
             predicate |= (predicate_or.inject(:|) & predicate_and.inject(:&)) if predicate_or.present? && predicate_and.present?
             query = query.where(predicate)
           end
@@ -78,19 +78,19 @@ module RailsAdmin
           return false
         end
 
-        
+
         private
-        
+
         def check_associations(abstract_model, tree = [])
           tree << abstract_model.model.name
           abstract_model.belongs_to_associations.each do |assoc|
-            tree << assoc[:parent_model].name     
+            tree << assoc[:parent_model].name
             check_associations(RailsAdmin::AbstractModel.new(assoc[:parent_model].name))
           end
-          
+
           return (self.models.collect{|m| m.name} & tree).length > 0
         end
-        
+
         # Retrieve the association hierarchy for a model.
         # Ex: let's assume a model is defined as followed :
         # Field belongs_to Table belongs_to Company
@@ -104,7 +104,7 @@ module RailsAdmin
           abstract_model.belongs_to_associations.each do |assoc|
             next if not assoc[:parent_model].respond_to? :name
             current_abstract_model = RailsAdmin::AbstractModel.new(assoc[:parent_model].name)
-            if not check_associations(current_abstract_model)            
+            if not check_associations(current_abstract_model)
               current_model_associations[current_association_name] << current_abstract_model.model.name
             else
               current_model_associations[current_association_name] << retrieve_associations_tree(current_abstract_model, current_abstract_model.model.name)
@@ -142,13 +142,13 @@ module RailsAdmin
           end
           return {}
         end
-                        
+
         module ControllerExtension
-          
+
           def scope_adapter
             @scope_adapter
           end
-          
+
           def current_scope
             @current_scope
           end
@@ -167,34 +167,34 @@ module RailsAdmin
               format.js {render :partial => 'rails_admin/extensions/scope/scope_selector', :locals => {:models => @scope_adapter.models}}
             end
           end
-          
+
           def list_entries_for(model_name, association = {})
             abstract_model = RailsAdmin::AbstractModel.new(model_name)
             scope = @authorization_adapter && @authorization_adapter.query(:list, abstract_model)
             abstract_model.where(association, scope)
           end
-                    
+
           def get_scope_models
             @current_scope = {}
             session[:scope] ||= {}
             parent_model     = nil
             parent_selection_id = nil
-            @scope_adapter.models.each do |model|                
+            @scope_adapter.models.each do |model|
               model_name = model.name
-              association = parent_model && parent_selection_id ? {"#{parent_model.table_name.singularize}_id" => parent_selection_id} : parent_entries.first.id rescue nil || {} 
+              association = parent_model && parent_selection_id ? {"#{parent_model.table_name.singularize}_id" => parent_selection_id} : parent_entries.first.id rescue nil || {}
               entries = list_entries_for(model_name, association)
               if(entries.reject{|e| e.id != session[:scope][model_name].to_i}.length == 1)
                 selection = params[model.name] || session[:scope][model_name]
                 update_session_for_model(model, selection)
               else #reset
-                selection = params[model.name].to_s.length > 0 ? params[model.name] : entries.first.id rescue nil
+                selection = params[model.name].to_s.length > 0 ? params[model.name] : (current_user.company || entries.first).id rescue nil
                 update_session_for_model(model, selection)
               end
-              
+
               @current_scope[model_name] = {:entries => entries, :selected  => selection, :selected_record => (entries[entries.index{|e| e.id == selection.to_i}] rescue nil)}
-                            
+
               self.instance_variable_set("@#{model.to_s.underscore}", @current_scope[model_name][:selected_record] )
-                                          
+
               #save the parent information so we can cascade reset if needed
               parent_model = model
               parent_entries = entries
