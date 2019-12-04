972324c8fceb7577907f20eef2135da34f863f75
daniellawton <daniel.lawton@fraudscreen.co.uk>
2019-12-04 14:51:18 +0000

    CMOSD-1131: Use and display the default language when the browser language does not match an existing locale

--- app/controllers/rails_admin/application_controller.rb
+++ app/controllers/rails_admin/application_controller.rb
@@ -90,7 +90,13 @@ module RailsAdmin
     end
 
     def _set_locale
-      I18n.locale = get_locale || extract_locale_from_accept_language_header
+      unless extract_locale_from_accept_language_header.nil? && get_locale.nil?
+        if I18n.available_locales.index(get_locale || extract_locale_from_accept_language_header.to_sym)
+          I18n.locale = get_locale || extract_locale_from_accept_language_header
+        else
+          I18n.locale = I18n.default_locale
+        end
+      end
     end
 
 
