0e2ebeaf81c465e13c64368267a6c99f66edcff7
Ilja Ijevlevs <ilja.ijevlevs@gdslink.com>
2020-05-25 14:18:42 +0100

    CMOSD-2062 Asset: An error appears upon clicking Submit without selecting any file in asset

--- lib/rails_admin/config/actions/asset_action.rb
+++ lib/rails_admin/config/actions/asset_action.rb
@@ -24,68 +24,72 @@ module RailsAdmin
               end
 
             elsif request.put? # UPDATE
-              tempFile = params[:picture].tempfile
-              file = File.open(tempFile)
-              picture_asset = PictureAsset.new
-              picture_asset.data_file_name = params[:picture].original_filename
-              picture_asset.data_content_type = params[:picture].content_type
-              if(["image/png", "image/jpeg", "image/jpg", "image/gif", "image/tiff"].include? picture_asset.data_content_type)
-                if(CaseCenter::Config::Reader.get('mongodb_attachment_database'))
-                  Mongoid.override_client(:attachDb)
-                end
-                grid_fs = Mongoid::GridFS
-                thumbFilename = params[:picture].original_filename
-                line = Terrapin::CommandLine.new("convert", ":in -scale :resolution :out")
-                line.run(in: tempFile.path, resolution: "30x30", out: thumbFilename)
-                thumbFile = File.open(thumbFilename)
-
-
-                #Encryption
-                public_key_file = CaseCenter::Config::Reader.get('attachments_public_key');
-                public_key = OpenSSL::PKey::RSA.new(File.read(public_key_file))
-                cipher = OpenSSL::Cipher.new('aes-256-cbc')
-                cipher.encrypt
-                key = cipher.random_key
-                encThumbData = cipher.update(File.read(thumbFile))
-                File.open(thumbFile, 'wb') do |f|
-                  f.write(encThumbData)
-                end
-                encData = cipher.update(File.read(file))
-                File.open(file, 'wb') do |f|
-                  f.write(encData)
-                end
-                encrypted_aes = Base64.encode64(public_key.public_encrypt(key))
-                picture_asset.aes_key = encrypted_aes
-
-                #End of encryption block
-                grid_file = grid_fs.put(file.path)
-                picture_asset.data_file_size = File.size(tempFile).to_i
-                picture_asset.assetable_id = params[:Company].to_i
-                picture_asset.image_id = grid_file.id
-                grid_thumb_file = grid_fs.put(thumbFile.path)
-                picture_asset.thumb_image_id = grid_thumb_file.id
-                Mongoid.override_client(:default)
-                thumbFile.close
-                File.delete(thumbFile.path)
-                if picture_asset.save
-                  @auditing_adapter && @auditing_adapter.create_object(picture_asset, @abstract_model, _current_user)
-                  respond_to do |format|
-                    format.html { redirect_to_on_success }
-                    format.js { render json: {id: picture_asset.id.to_s, label: @model_config.with(object: picture_asset).object_label} }
-                  end
-                else
+              if params[:picture]              
+                tempFile = params[:picture].tempfile
+                file = File.open(tempFile)
+                picture_asset = PictureAsset.new
+                picture_asset.data_file_name = params[:picture].original_filename
+                picture_asset.data_content_type = params[:picture].content_type
+                if(["image/png", "image/jpeg", "image/jpg", "image/gif", "image/tiff"].include? picture_asset.data_content_type)
                   if(CaseCenter::Config::Reader.get('mongodb_attachment_database'))
                     Mongoid.override_client(:attachDb)
                   end
-                  grid_fs.delete(picture_asset.image_id)
-                  grid_fs.delete(picture_asset.thumb_image_id)
-                  Mongoid.override_client(:default)
-                  picture_asset.errors.full_messages.each do |message|
-                    flash[:error] = message
+                  grid_fs = Mongoid::GridFS
+                  thumbFilename = params[:picture].original_filename
+                  line = Terrapin::CommandLine.new("convert", ":in -scale :resolution :out")
+                  line.run(in: tempFile.path, resolution: "30x30", out: thumbFilename)
+                  thumbFile = File.open(thumbFilename)
+
+
+                  #Encryption
+                  public_key_file = CaseCenter::Config::Reader.get('attachments_public_key');
+                  public_key = OpenSSL::PKey::RSA.new(File.read(public_key_file))
+                  cipher = OpenSSL::Cipher.new('aes-256-cbc')
+                  cipher.encrypt
+                  key = cipher.random_key
+                  encThumbData = cipher.update(File.read(thumbFile))
+                  File.open(thumbFile, 'wb') do |f|
+                    f.write(encThumbData)
                   end
+                  encData = cipher.update(File.read(file))
+                  File.open(file, 'wb') do |f|
+                    f.write(encData)
+                  end
+                  encrypted_aes = Base64.encode64(public_key.public_encrypt(key))
+                  picture_asset.aes_key = encrypted_aes
+
+                  #End of encryption block
+                  grid_file = grid_fs.put(file.path)
+                  picture_asset.data_file_size = File.size(tempFile).to_i
+                  picture_asset.assetable_id = params[:Company].to_i
+                  picture_asset.image_id = grid_file.id
+                  grid_thumb_file = grid_fs.put(thumbFile.path)
+                  picture_asset.thumb_image_id = grid_thumb_file.id
+                  Mongoid.override_client(:default)
+                  thumbFile.close
+                  File.delete(thumbFile.path)
+                  if picture_asset.save
+                    @auditing_adapter && @auditing_adapter.create_object(picture_asset, @abstract_model, _current_user)
+                    respond_to do |format|
+                      format.html { redirect_to_on_success }
+                      format.js { render json: {id: picture_asset.id.to_s, label: @model_config.with(object: picture_asset).object_label} }
+                    end
+                  else
+                    if(CaseCenter::Config::Reader.get('mongodb_attachment_database'))
+                      Mongoid.override_client(:attachDb)
+                    end
+                    grid_fs.delete(picture_asset.image_id)
+                    grid_fs.delete(picture_asset.thumb_image_id)
+                    Mongoid.override_client(:default)
+                    picture_asset.errors.full_messages.each do |message|
+                      flash[:error] = message
+                    end
+                  end
+                else
+                  flash[:error] = "Upload must be an image"
                 end
               else
-                flash[:error] = "Upload must be an image"
+                flash[:error] = I18n.t('asset_action_no_file')   
               end
             end
           end
