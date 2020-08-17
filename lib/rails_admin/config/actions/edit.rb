77fafaeb19c0a3d46cf407cfbb4fb2c7ee0ee886
Frederic VENOT <fred@venot.com>
2020-08-17 15:07:02 +0200

    CMOSD-2112, CMOSD-1998
    remove binding.pry

--- lib/rails_admin/config/actions/edit.rb
+++ lib/rails_admin/config/actions/edit.rb
@@ -83,7 +83,7 @@ module RailsAdmin
                     file = File.open(tempFile)
 
                     currentFileType = Terrapin::CommandLine.new('file', '-b --mime-type :file').run(file: tempFile.path).strip
-binding.pry
+
                     if ["application/rtf","text/rtf","text/csv","text/plain","application/csv","application/vnd.ms-excel"].index(currentFileType) != nil
                       if(CaseCenter::Config::Reader.get('mongodb_attachment_database'))
                         Mongoid.override_client(:attachDb)
