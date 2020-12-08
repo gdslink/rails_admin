b4d492b71c7146a36b9cb3115db273300d047a55
Ilja Ijevlevs <ilja.ijevlevs@fraudscreen.co.uk>
2020-12-08 20:34:11 +0000

    CMOSD-2237 XSL Sheets: RailsAdmin::ActionNotAllowed error when clicking on Save and Add another button

--- app/controllers/rails_admin/main_controller.rb
+++ app/controllers/rails_admin/main_controller.rb
@@ -364,6 +364,20 @@ module RailsAdmin
           redirectUrl =  "/admin/pattern?#{@current_scope_parameters.to_query}&locale=#{cur_locale}"
         end
         redirect_to redirectUrl, flash: redirectNotice
+      when "xsl_sheet"
+        redirectUrl = "/admin/xsl_sheet"
+        cur_locale = locale.to_s rescue 'en'
+        redirectNotice = {success: notice}
+        if params[:_add_edit]
+          redirectUrl = "/admin/xsl_sheet/#{object.id}/edit?#{@current_scope_parameters.to_query}&locale=#{cur_locale}"
+        end
+        if params[:_add_another]
+          redirectUrl =  "/admin/xsl_sheet/xsl_action?#{@current_scope_parameters.to_query}&locale=#{cur_locale}"
+        end
+        if params[:_save]
+          redirectUrl =  "/admin/xsl_sheet?#{@current_scope_parameters.to_query}&locale=#{cur_locale}"
+        end
+        redirect_to redirectUrl, flash: redirectNotice
       else
         if params[:_add_another]
           redirect_to new_path(@current_scope_parameters.merge(return_to: params[:return_to])), flash: {success: notice}
